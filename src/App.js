fd1f04e1686b95af61da9cfda05141d87477a8e9
Maria2009gift <kovalenkom2009@gmail.com>
2024-08-27 19:26:16 +0300

    image fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {List} from './components/List'
 
 const textName = "Maria"
 const picture = {
-  src: "./img/image.png",
+  src: "https://www.google.com/imgres?q=cat%20img%20gray&imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F858701710%2Fphoto%2Ffluffy-gray-kitten-of-a-russian-blue-cat.jpg%3Fs%3D612x612%26w%3D0%26k%3D20%26c%3DUNa1QGzOed4h-c5xs7bdNFA7UGKhjmaYDUn27f28RwA%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fgray-cat&docid=iuZ6mcFyhnW2eM&tbnid=emUI4PA9oTQijM&vet=12ahUKEwi1j822ypWIAxUeJxAIHQ3hMXwQM3oECCUQAA..i&w=612&h=408&hcb=2&ved=2ahUKEwi1j822ypWIAxUeJxAIHQ3hMXwQM3oECCUQAA",
   alt: "Cat"
 }
 
